2193764d3e620ddefe1a180254bd78f1565bb1b4
duduf <dudufdasilva@hotmail.com>
2025-10-07 20:49:19 -0300

    Desenvolvimento inicial dos arquivos referentes às telas inicial e de cadastro de aluno, além da criação de um css global ao programa do aluno

---
+++ js/index.js
@@ -0,0 +1,8 @@
+/**
+ * Redireciona o usuário para a página especificada.
+ * @param {string} page - O caminho para a página de destino.
+ */
+
+function navigateTo(page) {
+    window.location.href = page;
+}
