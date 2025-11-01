e90ec4e5aded58fa1bc6e0effb599491b3f61fba
eu.gili <gili.eliseup@gmail.com>
2025-10-31 22:01:59 -0300

    fase 1

--- sbu/backend/routes/alunos.js
+++ sbu/backend/routes/alunos.js
@@ -2,13 +2,10 @@ const express = require('express');
 const router = express.Router();
 const alunoController = require('../controllers/alunoController');
 
-// Cadastrar aluno
 router.post('/', alunoController.cadastrarAluno);
 
-// Listar todos os alunos
 router.get('/', alunoController.listarAlunos);
 
-// Buscar aluno por RA
 router.get('/ra/:ra', alunoController.buscarAlunoPorRa);
 
 module.exports = router;
